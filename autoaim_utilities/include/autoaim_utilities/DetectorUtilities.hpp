df858325b966f8584bc4e7826fef04905712b781
JellyfishKnight <1002984726@qq.com>
2023-10-30 17:22:12 +0800

    add autoaim interfaces

---
+++ autoaim_utilities/include/autoaim_utilities/DetectorUtilities.hpp
@@ -0,0 +1,16 @@
+// created by liuhan on 2023/10/29
+// Submodule of HeliosRobotSystem
+// for more see document: https://swjtuhelios.feishu.cn/docx/MfCsdfRxkoYk3oxWaazcfUpTnih?from=from_copylink
+/*
+ * ██   ██ ███████ ██      ██  ██████  ███████
+ * ██   ██ ██      ██      ██ ██    ██ ██
+ * ███████ █████   ██      ██ ██    ██ ███████
+ * ██   ██ ██      ██      ██ ██    ██      ██
+ * ██   ██ ███████ ███████ ██  ██████  ███████
+ */
+#pragma once
+
+#include "Armor.hpp"
+#include "NumberClassifier.hpp"
+#include "Omega.hpp"
+#include "PnPSolver.hpp"
