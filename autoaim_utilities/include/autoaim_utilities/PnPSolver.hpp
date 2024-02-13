4c304bb3f9d3f066df32429d23def569b6d707bd
jellyfishknight <1002984726@qq.com>
2024-02-13 18:16:25 +0800

    restruct pnp solver

--- autoaim_utilities/include/autoaim_utilities/PnPSolver.hpp
+++ autoaim_utilities/include/autoaim_utilities/PnPSolver.hpp
@@ -1,61 +1,160 @@
-// Copyright 2022 Chen Jun
-// Licensed under the MIT License.
 
-#ifndef ARMOR_DETECTOR__PNP_SOLVER_HPP_
-#define ARMOR_DETECTOR__PNP_SOLVER_HPP_
-
+#pragma once
+// ROS2
+#include <opencv2/core/matx.hpp>
+#include <opencv2/core/quaternion.hpp>
+#include <opencv2/core/types.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <angles/angles.h>
 #include <geometry_msgs/msg/point.hpp>
-#include <opencv2/core.hpp>
 
 // STL
 #include <array>
 #include <vector>
 
+// Eigen
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+// OpenCV
+#include <opencv2/core.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+
+// Ceres
+#include <ceres/ceres.h>
+
 #include "Armor.hpp"
 
 namespace helios_cv {
 
-typedef struct PnPParams {
-  // Small armor
-  double small_armor_width;
-  double small_armor_height;
-
-  // Large armor
-  double large_armor_width;
-  double large_armor_height;
-
-  // Energy armor
-  double energy_armor_width;
-  double energy_armor_height;
-}PnPParams;
-
+constexpr double small_armor_width = 135.0;
+constexpr double small_armor_height = 55.0;
+constexpr double large_armor_width = 225.0;
+constexpr double large_armor_height = 55.0;
+constexpr double energy_armor_width = 380.0;
+constexpr double energy_armor_height = 400.0;
 
 class PnPSolver
 {
 public:
   PnPSolver(
     const std::array<double, 9> & camera_matrix,
-    const std::vector<double> & distortion_coefficients,
-    const PnPParams& pnp_solver_params);
+    const std::vector<double> & distortion_coefficients);
 
   // Get 3d position
-  bool solvePnP(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec);
+  virtual bool solve_pose(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec);
 
   // Calculate the distance between armor center and image center
   float calculateDistanceToCenter(const cv::Point2f & image_point);
 
-  void update_params(const PnPParams& pnp_params);
+protected:
+  bool solve_pnp(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec);
 
   // Four vertices of armor in 3d
   std::vector<cv::Point3f> small_armor_points_;
   std::vector<cv::Point3f> large_armor_points_;
-  std::vector<cv::Point3f> energy_armor_points_;
-private:
+  std::vector<cv::Point3f> object_points_;
+
+  std::vector<cv::Point2f> image_armor_points;
+
   cv::Mat camera_matrix_;
   cv::Mat dist_coeffs_;
-  
-  PnPParams pnp_solver_params_;
+private:
+  rclcpp::Logger logger_ = rclcpp::get_logger("PnPSolver");
 };
-} // namespace helios_cv
 
-#endif  // ARMOR_DETECTOR__PNP_SOLVER_HPP_
+
+class ArmorProjectYaw : public PnPSolver {
+public:
+  explicit ArmorProjectYaw(
+    const std::array<double, 9> & camera_matrix, 
+    const std::vector<double> & dist_coeffs);
+
+  bool solve_pose(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec) override;
+  
+  void update_transform_info(const cv::Quatd& odom2cam_r, const cv::Quatd& cam2odom_r);
+
+  void draw_projection_points(cv::Mat& image);
+
+  bool use_projection = true;
+private:
+  struct CostFunctor {
+    template<typename T>
+    bool operator() (const T* const yaw, T* residual) const {
+      // Caculate pose in imu
+      Eigen::Matrix<T, 3, 3> rotation_mat, R_x, R_y, R_z;
+      R_x << T(1), T(0), T(0),
+              T(0), ceres::cos(T(pthis_->roll_)), -ceres::sin(T(pthis_->roll_)),
+              T(0), ceres::sin(T(pthis_->roll_)), ceres::cos(T(pthis_->roll_));
+      R_y << ceres::cos(T(pthis_->pitch_)), T(0), ceres::sin(T(pthis_->pitch_)),
+              T(0), T(1), T(0),
+              -ceres::sin(T(pthis_->pitch_)), T(0), ceres::cos(T(pthis_->pitch_));
+      R_z << ceres::cos(*yaw), -ceres::sin(*yaw), T(0),
+              ceres::sin(*yaw), ceres::cos(*yaw), T(0),
+              T(0), T(0), T(1);
+      rotation_mat = R_z * R_y * R_x;
+      // Convert pose to camera
+      Eigen::Matrix<T, 3, 3> odom2cam_r;
+      odom2cam_r << T(pthis_->odom2cam_r_(0, 0)), T(pthis_->odom2cam_r_(0, 1)), T(pthis_->odom2cam_r_(0, 2)),
+                  T(pthis_->odom2cam_r_(1, 0)), T(pthis_->odom2cam_r_(1, 1)), T(pthis_->odom2cam_r_(1, 2)),
+                  T(pthis_->odom2cam_r_(2, 0)), T(pthis_->odom2cam_r_(2, 1)), T(pthis_->odom2cam_r_(2, 2));
+      rotation_mat = odom2cam_r * rotation_mat;
+      // Project points
+      Eigen::Matrix<T, 3, 1> tvec;
+      tvec << T(pthis_->tvec_.at<double>(0, 0)), T(pthis_->tvec_.at<double>(1, 0)), T(pthis_->tvec_.at<double>(2, 0));
+      std::vector<Eigen::Vector<T, 3>> points;
+      for (const auto &point : pthis_->object_points_) {
+        Eigen::Matrix<T, 3, 1> point_eigen;
+        point_eigen << T(point.x), T(point.y), T(point.z);
+        points.emplace_back(rotation_mat * point_eigen + tvec);
+      }
+      Eigen::Matrix<T, 3, 3> camera_matrix ;
+      camera_matrix << T(pthis_->camera_matrix_.at<double>(0, 0)), T(pthis_->camera_matrix_.at<double>(0, 1)), T(pthis_->camera_matrix_.at<double>(0, 2)),
+                      T(pthis_->camera_matrix_.at<double>(1, 0)), T(pthis_->camera_matrix_.at<double>(1, 1)), T(pthis_->camera_matrix_.at<double>(1, 2)),
+                      T(pthis_->camera_matrix_.at<double>(2, 0)), T(pthis_->camera_matrix_.at<double>(2, 1)), T(pthis_->camera_matrix_.at<double>(2, 2));
+      Eigen::Matrix<T, 5, 1> dist_coeffs;
+      dist_coeffs << T(pthis_->dist_coeffs_.at<double>(0)), T(pthis_->dist_coeffs_.at<double>(1)), T(pthis_->dist_coeffs_.at<double>(2)), T(pthis_->dist_coeffs_.at<double>(3)), T(pthis_->dist_coeffs_.at<double>(4));
+      std::vector<Eigen::Vector<T, 3>> projected_points;
+      for (const auto &point : points) {
+        Eigen::Vector<T, 3> projected_point;
+        projected_point = camera_matrix / point[2] * point;
+        projected_points.emplace_back(projected_point);
+      }
+      // Caculate the difference between projected points and image points (Use Vector's angle)
+      auto get_angle = [](const Eigen::Vector<T, 3> &v1, const Eigen::Vector<T, 3> &v2) {
+          return ceres::acos(v1.dot(v2) / (v1.norm() * v2.norm()));
+      };
+      Eigen::Vector<T, 3> image_vector, projected_vector;
+      residual[0] = T(0);
+      for (int i = 0; i < 4; i++) {
+        image_vector << T(pthis_->image_armor_points[(i + 1) % 4].x - pthis_->image_armor_points[i].x), T(pthis_->image_armor_points[(i + 1) % 4].y - pthis_->image_armor_points[i].y), T(1);
+        projected_vector << projected_points[(i + 1) % 4][0] - projected_points[i][0], projected_points[(i + 1) % 4][1] - projected_points[i][1], T(1);
+        residual[0] += get_angle(image_vector, projected_vector);
+      }
+      return true;
+    }
+  };
+  bool is_transform_info_updated_ = false;
+
+  cv::Matx33d odom2cam_r_;
+  cv::Matx33d cam2odom_r_;
+
+  static ArmorProjectYaw* pthis_;
+
+  double diff_function(double yaw); 
+
+  [[deprecated]] double phi_optimization(double left, double right, double eps);
+
+  void get_rotation_matrix(double yaw, cv::Mat& rotation_mat) const;
+
+  std::vector<cv::Point2f> projected_points_;
+  cv::Mat tvec_;
+  // The pitch and roll of armor are fixed for target
+  double roll_ = 0, pitch_ = angles::from_degrees(15);
+  double armor_angle_;
+
+  rclcpp::Logger logger_ = rclcpp::get_logger("ArmorProjectYaw");
+};
+
+} // namespace helios_cv
--- autoaim_utilities/src/PnPsolver.cpp
+++ autoaim_utilities/src/PnPsolver.cpp
@@ -1,31 +1,26 @@
-// Copyright 2022 Chen Jun
-
 #include "PnPSolver.hpp"
 #include "Armor.hpp"
 
-#include <opencv2/calib3d.hpp>
+#include <rclcpp/logging.hpp>
 #include <vector>
-
 
 namespace helios_cv {
 
 PnPSolver::PnPSolver(
-  const std::array<double, 9> & camera_matrix, const std::vector<double> & dist_coeffs,
-  const PnPParams& pnp_solver_params)
+  const std::array<double, 9> & camera_matrix, const std::vector<double> & dist_coeffs)
 : camera_matrix_(cv::Mat(3, 3, CV_64F, const_cast<double *>(camera_matrix.data())).clone()),
-  dist_coeffs_(cv::Mat(1, 5, CV_64F, const_cast<double *>(dist_coeffs.data())).clone()),
-  pnp_solver_params_(pnp_solver_params)
+  dist_coeffs_(cv::Mat(1, 5, CV_64F, const_cast<double *>(dist_coeffs.data())).clone())
 {
   // Unit: m
   double half_y{};
   double half_z{};
 
   // Start from bottom left in clockwise order
-  // Model coordinate: x forward, y left, z up
+  // Model coordinate: x forward, y le`ft, z up
   // Small armor
   // Unit: m
-  half_y = pnp_solver_params.small_armor_width / 2.0 / 1000.0;
-  half_z = pnp_solver_params.small_armor_height / 2.0 / 1000.0;
+  half_y = small_armor_width / 2.0 / 1000.0;
+  half_z = small_armor_height / 2.0 / 1000.0;
 
   small_armor_points_.emplace_back(cv::Point3f(0, half_y, -half_z));
   small_armor_points_.emplace_back(cv::Point3f(0, half_y, half_z));
@@ -33,25 +28,16 @@ PnPSolver::PnPSolver(
   small_armor_points_.emplace_back(cv::Point3f(0, -half_y, -half_z));
 
   // Large armor
-  half_y = pnp_solver_params.large_armor_width / 2.0 / 1000.0;
-  half_z = pnp_solver_params.large_armor_height / 2.0 / 1000.0;
+  half_y = large_armor_width / 2.0 / 1000.0;
+  half_z = large_armor_height / 2.0 / 1000.0;
 
   large_armor_points_.emplace_back(cv::Point3f(0, half_y, -half_z));
   large_armor_points_.emplace_back(cv::Point3f(0, half_y, half_z));
   large_armor_points_.emplace_back(cv::Point3f(0, -half_y, half_z));
   large_armor_points_.emplace_back(cv::Point3f(0, -half_y, -half_z));
-
-  // Energy armor
-  half_y = pnp_solver_params.energy_armor_width / 2.0 / 1000.0;
-  half_z = pnp_solver_params.energy_armor_height / 2.0 / 1000.0;
-
-  energy_armor_points_.emplace_back(cv::Point3f(0, half_y, -half_z));
-  energy_armor_points_.emplace_back(cv::Point3f(0, half_y, half_z));
-  energy_armor_points_.emplace_back(cv::Point3f(0, -half_y, half_z));
-  energy_armor_points_.emplace_back(cv::Point3f(0, -half_y, -half_z));
 }
 
-bool PnPSolver::solvePnP(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec)
+bool PnPSolver::solve_pnp(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec)
 {
   std::vector<cv::Point2f> image_armor_points;
 
@@ -67,8 +53,6 @@ bool PnPSolver::solvePnP(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec)
     object_points = small_armor_points_;
   } else if (armor.type == ArmorType::LARGE) {
     object_points = large_armor_points_;
-  } else if (armor.type == ArmorType::ENERGY_TARGET) {
-    object_points = energy_armor_points_;
   } else {
     return false;
   }
@@ -78,11 +62,174 @@ bool PnPSolver::solvePnP(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec)
     cv::SOLVEPNP_IPPE);
 }
 
+bool PnPSolver::solve_pose(const Armor & armor, cv::Mat & rvec, cv::Mat & tvec) {
+  if (solve_pnp(armor, rvec, tvec)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 float PnPSolver::calculateDistanceToCenter(const cv::Point2f & image_point)
 {
   float cx = camera_matrix_.at<double>(0, 2);
   float cy = camera_matrix_.at<double>(1, 2);
   return cv::norm(image_point - cv::Point2f(cx, cy));
+}
+
+void ArmorProjectYaw::update_transform_info(const cv::Quatd& odom2cam_r, const cv::Quatd& cam2odom_r) {
+  odom2cam_r_ = odom2cam_r.toRotMat3x3();
+  cam2odom_r_ = cam2odom_r.toRotMat3x3();
+}
+
+
+double ArmorProjectYaw::diff_function(double yaw) {
+    // Caculate rotation matrix
+    cv::Mat rotation_matrix;
+    get_rotation_matrix(yaw, rotation_matrix);
+    rotation_matrix = odom2cam_r_ * rotation_matrix;
+    // Turn rotation matrix into rotation vector
+    // caculate projection points
+    cv::projectPoints(object_points_, rotation_matrix, tvec_, camera_matrix_, dist_coeffs_, projected_points_);
+    // Caculate the difference between projected points and image points by angle between vectors
+    double diff = 0;
+    Eigen::Vector3d image_vector[2], projected_vector[2];
+    image_vector[0] << image_armor_points[3].x - image_armor_points[0].x, image_armor_points[3].y - image_armor_points[0].y, 1;
+    image_vector[1] << image_armor_points[1].x - image_armor_points[2].x, image_armor_points[1].y - image_armor_points[2].y, 1;
+    projected_vector[0] << projected_points_[3].x - projected_points_[0].x, projected_points_[3].y - projected_points_[0].y, 1;
+    projected_vector[1] << projected_points_[1].x - projected_points_[0].x, projected_points_[1].y - projected_points_[0].y, 1;
+    Eigen::Vector3d image_cross = image_vector[0].cross(image_vector[1]);
+    Eigen::Vector3d projected_cross = projected_vector[0].cross(projected_vector[1]);
+    double raw_angle = image_vector[0].dot(image_vector[1]) / (image_vector[0].norm() * image_vector[1].norm());
+    double project_angle;
+    if (image_cross.dot(projected_cross) >= 0) {
+        project_angle = projected_vector[0].dot(projected_vector[1]) / (projected_vector[0].norm() * projected_vector[1].norm());
+    } else {
+        project_angle = -projected_vector[0].dot(projected_vector[1]) / (projected_vector[0].norm() * projected_vector[1].norm());
+    }
+    return raw_angle - project_angle;
+}
+
+void ArmorProjectYaw::draw_projection_points(cv::Mat& image) {
+    if (projected_points_.empty()) {
+        RCLCPP_DEBUG(logger_, "empty projection");
+        return;
+    }
+    for (int i = 0; i < 4; i++) {
+        cv::circle(image, projected_points_[i], i + 1, cv::Scalar(0, 0, 255), -1);
+        // cv::circle(image, image_points_[i], i + 1, cv::Scalar(255, 0, 0), 2);
+    }
+    cv::line(image, projected_points_[0], projected_points_[2], cv::Scalar(255, 255, 0), 2);
+    cv::line(image, projected_points_[1], projected_points_[3], cv::Scalar(255, 255, 0), 2);
+    cv::line(image, projected_points_[0], projected_points_[1], cv::Scalar(255, 255, 0), 2);
+    cv::line(image, projected_points_[1], projected_points_[2], cv::Scalar(255, 255, 0), 2);
+    cv::line(image, projected_points_[2], projected_points_[3], cv::Scalar(255, 255, 0), 2);
+    cv::line(image, projected_points_[3], projected_points_[0], cv::Scalar(255, 255, 0), 2);
+    projected_points_.clear();
+}
+
+double ArmorProjectYaw::phi_optimization(double left, double right, double eps) {
+  // Make fibonacci sequence
+  std::vector<double> fn(2);
+  std::vector<double> fib_seq;
+  fn[0] = 1, fn[1] = 1;
+  double f_lambda = 0, f_mu = 0;
+  int fib_cnt;
+  for (fib_cnt = 2; fn[fib_cnt - 1] < 1.0 / eps; fib_cnt++) {
+    fn.push_back(fn[fib_cnt - 2] + fn[fib_cnt - 1]);
+  }
+  fib_cnt--;
+  double lambda = left + (fn[fib_cnt - 2] / fn[fib_cnt]) * (right - left);
+  double mu = left + (fn[fib_cnt - 1] / fn[fib_cnt]) * (right - left);
+  f_lambda = diff_function(lambda);
+  f_mu = diff_function(mu);
+  for (int i = 0; i < fib_cnt - 2; i++) {
+    if (f_lambda < f_mu) {
+      right = mu;
+      mu = lambda;
+      f_mu = f_lambda;
+      lambda = left + (fn[fib_cnt - i - 3] / fn[fib_cnt - i - 1]) * (right - left);
+      f_lambda = diff_function(lambda);
+    } else {
+      left = lambda;
+      lambda = mu;
+      f_lambda = f_mu;
+      mu = left + (fn[fib_cnt - i - 2] / fn[fib_cnt - i - 1]) * (right - left);
+      f_mu = diff_function(mu);
+    }
+  }
+  f_mu = diff_function(mu);
+  if (f_lambda < f_mu) {
+    right = mu;
+  } else {
+    left = lambda;
+  }
+  return 0.5 * (left + right);
+}
+
+void ArmorProjectYaw::get_rotation_matrix(double yaw, cv::Mat& rotation_mat) const {
+  // caculate rotation matrix
+  // for more see paper: https://danceswithcode.net/engineeringnotes/rotations_in_3d/rotations_in_3d_part1.html
+  // Calculate rotation about x axis
+  cv::Mat R_x = (cv::Mat_<double>(3,3) <<
+              1,       0,               0,
+              0,       std::cos(roll_), -std::sin(roll_),
+              0,       std::sin(roll_), std::cos(roll_));
+    
+  // Calculate rotation about y axis
+  cv::Mat R_y = (cv::Mat_<double>(3,3) <<
+              std::cos(pitch_),    0,      std::sin(pitch_),
+              0,                   1,      0,
+              -std::sin(pitch_),   0,      std::cos(pitch_));
+    
+  // Calculate rotation about z axis
+  cv::Mat R_z = (cv::Mat_<double>(3,3) <<
+              std::cos(yaw),    -std::sin(yaw),      0,
+              std::sin(yaw),    std::cos(yaw),       0,
+              0,                0,                   1);
+  rotation_mat = R_z * R_y * R_x;
+}
+
+bool ArmorProjectYaw::solve_pose(const Armor &armor, cv::Mat &rvec, cv::Mat &tvec) {
+  /*Solve PnP First To Get Position Info*/
+  // Fill in image points
+  if (!use_projection) {
+    return solve_pnp(armor, rvec, tvec);
+  } else {
+    if (!solve_pnp(armor, rvec, tvec)) {
+      return false;
+    }
+  }
+  if (!is_transform_info_updated_) {
+    RCLCPP_WARN(logger_, "PnP Solve done, but transform info not updated, skipping");
+    return true;
+  }
+  /*Start Reprojection*/
+  double yaw = 0;
+  tvec_ = tvec;
+  armor_angle_ = armor.angle;
+  // Choose pitch value
+  if (armor.number == "outpost") {
+      pitch_ = angles::from_degrees(-15.0);
+  } else {
+      pitch_ = angles::from_degrees(15.0);
+  }
+  // Get min diff yaw
+  ceres::Problem problem;
+  ceres::CostFunction* costfunctor = new ceres::AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+  problem.AddResidualBlock(costfunctor, nullptr, &yaw);
+  ceres::Solver::Options options;
+  options.linear_solver_type = ceres::LinearSolverType::DENSE_QR;
+  options.minimizer_progress_to_stdout = false;
+  options.max_num_iterations = 100;
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+  // Get draw points
+  diff_function(yaw);
+  // Caculate rotation matrix
+  get_rotation_matrix(yaw, rvec);
+  rvec = odom2cam_r_ * rvec;
+  return true;
 }
 
 } // namespace helios_cv
